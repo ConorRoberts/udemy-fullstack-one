f411c6c2a23118a31b12816e82edf3f8cb3b8529
Conor Roberts <62777912+ConorRoberts@users.noreply.github.com>
2021-01-08 00:37:38 -0500

    Update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hey: "pal" });
+  res.send({ hey: "guy" });
 });
 
 const PORT = process.env.PORT;
