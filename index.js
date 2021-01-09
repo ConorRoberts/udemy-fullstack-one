6783fcec94f236cd061d4d14e5d228baa91525ce
Conor Roberts <62777912+ConorRoberts@users.noreply.github.com>
2021-01-09 00:58:02 -0500

    Update

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 import express from "express";
 import passport from "passport";
 import GoogleStrategy from "passport-google-oauth20";
-import { googleClientID, googleClientSecret } from "./config/keys";
 
-// Client ID: 579553257300-qti9k1bvkfs5onsf0qgs92o4p8jpq8m9.apps.googleusercontent.com
-// Secret: FZ2__u1x5ObJW0ctDEddSGqh
+// const keys = require("./config/keys");
+import * as keys from "./config/keys.js";
 
 const app = express();
 
 passport.use(
   new GoogleStrategy(
     {
-      clientID: googleClientID,
-      clientSecret: googleClientSecret,
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
     },
     (accessToken) => {
       console.log(accessToken);
     }
   )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
 );
 
 const PORT = process.env.PORT;
